76821b9829660998bd5714feddcaafbdf21ea3ca
ChuEating <chueating1005.cs11@nycu.edu.tw>
2024-02-29 14:48:58 +0800

    lab0: Environment Setup

---
+++ lab0/obj_dir/testbench.d
@@ -0,0 +1,9 @@
+testbench.o: ../part1/testbench.cpp \
+  /usr/local/Cellar/verilator/5.018/share/verilator/include/verilated.h \
+  /usr/local/Cellar/verilator/5.018/share/verilator/include/verilatedos.h \
+  /usr/local/Cellar/verilator/5.018/share/verilator/include/verilated_config.h \
+  /usr/local/Cellar/verilator/5.018/share/verilator/include/verilated_types.h \
+  /usr/local/Cellar/verilator/5.018/share/verilator/include/verilated_funcs.h \
+  /usr/local/Cellar/verilator/5.018/share/verilator/include/verilated_vcd_c.h \
+  /usr/local/Cellar/verilator/5.018/share/verilator/include/verilated_trace.h \
+  VfullAdder.h
